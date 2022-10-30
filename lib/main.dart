6352667fd5676acc4494c44476853c76bfaeeb5f
Farha092 <farhafaija592@gmail.com>
2022-10-30 18:43:28 +0600

    test

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,6 @@ import 'package:expense_app/constants.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 
-
-
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
